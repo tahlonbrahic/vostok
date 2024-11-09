c1c69ae295189622b0491c50db465a930e27011e
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-11-09 06:05:23 -0600

    Start initial none-ls configuration

---
+++ config/lsp/none-ls.nix
@@ -0,0 +1,37 @@
+{
+  plugins.none-ls = {
+    enable = true;
+    sources = {
+      code_actions = {
+        statix.enable = true;
+      };
+
+      diagnostics = {
+        stylelint.enable = true;
+        statix.enable = true;
+        sqlfluff.enable = true;
+        rubocop.enable = true;
+        pylint.enable = true;
+        mypy.enable = true;
+        markdownlint.enable = true;
+        hadolint.enable = true;
+        fish.enable = true;
+        dotenv_linter.enable = true;
+        deadnix.enable = true;
+        trail_space.enable = true;
+      };
+
+      formatting = {
+        stylua.enable = true;
+        alejandra.enable = true;
+        shfmt.enable = true;
+        gofmt.enable = true;
+        black.enable = true;
+        isort.enable = true;
+        prettier.enable = true;
+        prettier.disableTsServerFormatter = true;
+        typstfmt.enable = true;
+      };
+    };
+  };
+}
