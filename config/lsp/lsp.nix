11f7c22b82abacc9053759591a6e91a21ae5539e
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-30 18:00:50 -0600

    debugging ts_query_ls preventing build

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -23,7 +23,6 @@
         powershell_es.enable = true;
         powershell_es.package = inputs.fuyuNoNur.packages.${system}.powershell_es;
         yamlls.enable = true;
-        ts_query_ls.package = null;
       };
     };
     lsp-format.enable = true;
