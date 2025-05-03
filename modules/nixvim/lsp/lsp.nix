c16ae342d25e25623afa598be2662d8294463057
TahlonBrahic <tahlonbrahic@gmail.com>
2025-05-03 17:57:19 -0400

    remove upstream nil_ls

--- modules/nixvim/lsp/lsp.nix
+++ modules/nixvim/lsp/lsp.nix
@@ -16,27 +16,7 @@
         jsonls.enable = true;
         lua_ls.enable = true;
         marksman.enable = true; # Markdown
-        nil_ls = let
-          version = "2e24c9834e3bb5aa2a3701d3713b43a6fb106362";
-        in {
-          enable = true;
-          package = pkgs.nil.overrideAttrs (oldAttrs: rec {
-            pname = "nil";
-            inherit version;
-            src = pkgs.fetchFromGitHub {
-              owner = "oxalica";
-              repo = "nil";
-              rev = version;
-              sha256 = "sha256-DCIVdlb81Fct2uwzbtnawLBC/U03U2hqx8trqTJB7WA=";
-            };
-
-            cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
-              name = "${pname}-vendor.tar.gz";
-              inherit src;
-              outputHash = "sha256-FppdLgciTzF6tBZ+07IEzk5wGinsp1XUE7T18DCGvKg=";
-            });
-          });
-        };
+        nil_ls.enable = true;
         pyright.enable = true; # Python
         solargraph.enable = true; # Ruby
         taplo.enable = true; # TOML
